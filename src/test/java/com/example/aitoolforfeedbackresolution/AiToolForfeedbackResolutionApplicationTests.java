a5738746001370ec42e434258637709a97c373cb
akshay.s2 <akshay.s2@infoedge.com>
2025-08-21 14:33:22 +0530

    MERGE:

---
+++ src/test/java/com/example/aitoolforfeedbackresolution/AiToolForfeedbackResolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitoolforfeedbackresolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolForfeedbackResolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
